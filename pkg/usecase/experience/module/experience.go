bf8983c9116aaacd215046f76cbb37e8caa461ac
geshtng <sihotangg@gmail.com>
2019-11-28 09:35:34 +0700

    [Experiene] Change struct and response message

--- pkg/usecase/experience/module/experience.go
+++ pkg/usecase/experience/module/experience.go
@@ -6,7 +6,7 @@ import (
 	"github.com/TobaTourism/pkg/models"
 )
 
-func (u *experience) GetAllExperience() (models.Response, error) {
+func (u *experience) GetAllExperience() (models.ExperienceResponse, error) {
 	allExperience, err := u.experienceRepo.GetAllExperience()
 	if err != nil {
 		log.Println("[Usecase][Experience][GetAllExperience] Error: ", err)
@@ -17,7 +17,7 @@ func (u *experience) GetAllExperience() (models.Response, error) {
 	return allExperience, nil
 }
 
-func (u *experience) GetExperienceByID(experienceID int64) (models.Response, error) {
+func (u *experience) GetExperienceByID(experienceID int64) (models.ExperienceResponse, error) {
 	experience, err := u.experienceRepo.GetExperienceByID(experienceID)
 	if err != nil {
 		log.Println("[Usecase][Experience][GetExperienceByID] Error: ", err)
@@ -28,7 +28,7 @@ func (u *experience) GetExperienceByID(experienceID int64) (models.Response, err
 	return experience, nil
 }
 
-func (u *experience) CreateExperience(description, lokasi string) (models.Response, error) {
+func (u *experience) CreateExperience(description, lokasi string) (models.ExperienceResponse, error) {
 	experience, err := u.experienceRepo.CreateExperience(description, lokasi)
 	if err != nil {
 		log.Println("[Usecase][Experience][CreateExperience] Error: ", err)
@@ -39,7 +39,7 @@ func (u *experience) CreateExperience(description, lokasi string) (models.Respon
 	return experience, nil
 }
 
-func (u *experience) UpdateExperience(experienceID int64, description, lokasi string) (models.Response, error) {
+func (u *experience) UpdateExperience(experienceID int64, description, lokasi string) (models.ExperienceResponse, error) {
 	experience, err := u.experienceRepo.UpdateExperience(experienceID, description, lokasi)
 	if err != nil {
 		log.Println("[Usecase][Experience][UpdateExperience] Error: ", err)
@@ -50,7 +50,7 @@ func (u *experience) UpdateExperience(experienceID int64, description, lokasi st
 	return experience, nil
 }
 
-func (u *experience) DeleteExperience(experienceID int64) (models.Response, error) {
+func (u *experience) DeleteExperience(experienceID int64) (models.ExperienceResponse, error) {
 	experience, err := u.experienceRepo.DeleteExperience(experienceID)
 	if err != nil {
 		log.Println("[Usecase][Experience][DeleteExperience] Error: ", err)
